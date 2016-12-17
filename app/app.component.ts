2a17c8e00506f6ee1f416651ceec00bac41b0ecc
lizhonghui <lzhspace@gmail.com>
2016-12-17 19:59:43 +0800

    refactor: html分离

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,30 +4,7 @@ import { LoggerService } from './logger.service';
 
 @Component({
   selector: 'my-app',
-  template: `
-  <div>
-    <h1>根组件</h1>
-    <p>
-      嘿嘿，{{ greeting }}！
-      <label>
-        <input type="checkbox" [(ngModel)]="isShowMore">
-        是否显示详细信息
-      </label>
-    </p>
-    <p highlight *ngIf="isShowMore">Angular 2 是 Google 推出的新一代的Web开发框架</p>
-    <my-child [message]="msgToChild" (outer)="receive($event)"></my-child>
-    <p>从子组件获得的消息：{{ msgFromChild || '暂无' }}</p>
-  </div>
-  `,
-  styles:[
-    `div{
-      background:#A1DE93;
-      border-radius:5px;
-      width:500px;height:300px;
-      margin:100px auto;
-      padding:20px;
-    }`
-  ]
+  templateUrl: './app/app.component.html'
 })
 export class AppComponent {
   private greeting: string;
@@ -39,7 +16,7 @@ export class AppComponent {
 
   ngOnInit() {
     this.greeting = 'Angular 2';
-    this.msgToChild = 'msg from parent';
+    this.msgToChild = 'message from parent';
     this.logger.debug('应用已初始化');
   }
 
--- app/child.component.ts
+++ app/child.component.ts
@@ -2,22 +2,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'my-child',
-  template: `
-    <div>
-      <h1>子组件</h1>
-      <p>嘿嘿，我从父组件获取的值是：{{ message }}</p>
-      <button (click)="sendToParent()">发送到父组件</button>
-    </div>
-  `,
-  styles:[
-    `div {
-      border:1px solid #ccc;
-      border-radius:5px;
-      background-color:#70A1D7;
-      padding: 10px;
-      margin: 20px 0;
-    }`
-  ]
+  templateUrl: './app/child.component.html'
 })
 export class ChildComponent {
   @Input() private message: string;
